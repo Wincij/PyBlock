cb7ad4ee6d3d518cbf086d2e2d10fcfb4fe0420a
Wincij <m.pajaczkowski95@gmail.com>
2018-12-17 00:56:36 +0100

    Mini auto-test created

--- Test.py
+++ Test.py
@@ -3,6 +3,19 @@ from user import *
 import random
 
 
+def randomTransaction(users):
+
+    rand1 = random.randint(0,4)
+    rand2 = random.randint(5,9)
+    sender = users[rand1]
+    receiver = users[rand2]
+    amount = random.randint(1,20)
+    if sender.wallet < amount:
+        while sender.wallet < amount:
+            amount = random.randint(1,20)
+    sender.wallet-= amount
+    receiver.wallet+= amount
+    return {"sender":sender.name, "receiver": receiver.name, "amount": amount}
 
 
 
@@ -12,7 +25,6 @@ chain = [PyBlock(0, 364)]
 # Random user list
 Names = ['Ada','Ana','Thomas','Robert','Caroline', 'Joe','Lois','Ben','Amanda','John']
 users = []
-print(len(users))
 
 #
 for i in range(10):
@@ -20,8 +32,28 @@ for i in range(10):
     users.append(User(Names[i], wallet))
     print("Created {User} with {Wallet}".format(User=users[i].name, Wallet = users[i].wallet))
 
+# print(len(users))
+
 for i in range(3):
+    activeBlock = chain[i]
+    z= 0
+    while(len(activeBlock.transactionData)<=300):
+        temp = randomTransaction(users)
+        sender = temp.get("sender")
+        receiver = temp["receiver"]
+        amount = temp["amount"]
+        # print(str(z) + ". " + sender + " -> " + receiver + " ({amount})".format(amount = amount))
+        activeBlock.__newTransaction__(sender, receiver, amount)
+        z+=1
+
+
 
     newBlock = PyBlock(chain[i].Hash, proofOfWork(chain[i].Hash, chain[i].proofOfWork))
     chain.append(newBlock)
     print("Previous Block was Hashed with {hash2}\nBlock: {index} | Hashed with SHA256: {hash} \n\n".format(index = i, hash=str(hex(chain[i].Hash)), hash2=str(hex(chain[i].prevHash))))
+
+
+print("After transactions;")
+for i in range(10):
+
+    print("{User} with {Wallet}".format(User=users[i].name, Wallet = users[i].wallet))
